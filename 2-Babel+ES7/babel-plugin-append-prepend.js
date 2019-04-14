0fb7deb7e5a9fb76eb1e24879864b47b53891900
Yana Kalandarava <yana_kalandarava@epam.com>
2019-04-14 20:22:01 +0300

    [FC-2]: add babel plugin to support "append/prepend" methods

---
+++ 2-Babel+ES7/babel-plugin-append-prepend.js
@@ -0,0 +1,33 @@
+/**
+ *
+ *  Transform node1.append(node2) into node1.appendChild(node2)
+ *  to support ie 11 and older (also works for prepend)
+ *
+ *  It only works if you pass one(!) Node, not DOMString.
+ *
+ */
+
+module.exports = () => {
+
+    const isCorrectExpression = (path) => {
+        let {parentPath} = path;
+
+        if (parentPath.isCallExpression() && parentPath.get("arguments").length === 1) return true;
+    };
+
+    return {
+        visitor: {
+            MemberExpression(path) {
+                if (!isCorrectExpression(path)) return;
+
+                if (path.node.property.name === 'append') {
+                    path.node.property.name = 'appendChild';
+                }
+
+                if (path.node.property.name === 'prepend') {
+                    path.node.property.name = 'prependChild';
+                }
+            }
+        }
+    };
+};
